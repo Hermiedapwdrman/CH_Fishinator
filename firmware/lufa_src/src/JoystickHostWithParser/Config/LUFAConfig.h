55a674a4c93cc57ece697f282b8b8f761432fd07
patrick <re.craighospital@gmail.org>
2017-12-20 11:14:33 -0700

    Initial Commit for CH_Fishinator

--- firmware/lufa_src/src/JoystickHostWithParser/Config/LUFAConfig.h
+++ firmware/lufa_src/src/JoystickHostWithParser/Config/LUFAConfig.h
@@ -58,28 +58,28 @@
 
 		/* General USB Driver Related Tokens: */
 //		#define ORDERED_EP_CONFIG
-		#define USE_STATIC_OPTIONS               (USB_DEVICE_OPT_FULLSPEED | USB_OPT_REG_ENABLED | USB_OPT_AUTO_PLL)
-		#define USB_DEVICE_ONLY
-//		#define USB_HOST_ONLY
+		#define USE_STATIC_OPTIONS               (USB_OPT_REG_ENABLED | USB_OPT_AUTO_PLL)
+//		#define USB_DEVICE_ONLY
+		#define USB_HOST_ONLY
 //		#define USB_STREAM_TIMEOUT_MS            {Insert Value Here}
 //		#define NO_LIMITED_CONTROLLER_CONNECT
 //		#define NO_SOF_EVENTS
 
 		/* USB Device Mode Driver Related Tokens: */
 //		#define USE_RAM_DESCRIPTORS
-		#define USE_FLASH_DESCRIPTORS
+//		#define USE_FLASH_DESCRIPTORS
 //		#define USE_EEPROM_DESCRIPTORS
 //		#define NO_INTERNAL_SERIAL
-		#define FIXED_CONTROL_ENDPOINT_SIZE      8
-		#define DEVICE_STATE_AS_GPIOR            0
-		#define FIXED_NUM_CONFIGURATIONS         1
+//		#define FIXED_CONTROL_ENDPOINT_SIZE      {Insert Value Here}
+//		#define DEVICE_STATE_AS_GPIOR            {Insert Value Here}
+//		#define FIXED_NUM_CONFIGURATIONS         {Insert Value Here}
 //		#define CONTROL_ONLY_DEVICE
-		#define INTERRUPT_CONTROL_ENDPOINT
+//		#define INTERRUPT_CONTROL_ENDPOINT
 //		#define NO_DEVICE_REMOTE_WAKEUP
 //		#define NO_DEVICE_SELF_POWER
 
 		/* USB Host Mode Driver Related Tokens: */
-//		#define HOST_STATE_AS_GPIOR              0
+//		#define HOST_STATE_AS_GPIOR              {Insert Value Here}
 //		#define USB_HOST_TIMEOUT_MS              {Insert Value Here}
 //		#define HOST_DEVICE_SETTLE_DELAY_MS	     {Insert Value Here}
 //		#define NO_AUTO_VBUS_MANAGEMENT
--- firmware/lufa_src/src/MassStorageHost/MassStorageHost.h
+++ firmware/lufa_src/src/MassStorageHost/MassStorageHost.h
@@ -30,23 +30,24 @@
 
 /** \file
  *
- *  Header file for USBtoSerial.c.
+ *  Header file for MassStorage.c.
  */
 
-#ifndef _USB_SERIAL_H_
-#define _USB_SERIAL_H_
+#ifndef _MASS_STORAGE_HOST_H_
+#define _MASS_STORAGE_HOST_H_
 
 	/* Includes: */
 		#include <avr/io.h>
 		#include <avr/wdt.h>
-		#include <avr/interrupt.h>
+		#include <avr/pgmspace.h>
 		#include <avr/power.h>
+		#include <avr/interrupt.h>
+		#include <ctype.h>
+		#include <stdio.h>
 
-		#include "Descriptors.h"
-
-		#include <LUFA/Drivers/Board/LEDs.h>
+		#include <LUFA/Drivers/Misc/TerminalCodes.h>
 		#include <LUFA/Drivers/Peripheral/Serial.h>
-		#include <LUFA/Drivers/Misc/RingBuffer.h>
+		#include <LUFA/Drivers/Board/LEDs.h>
 		#include <LUFA/Drivers/USB/USB.h>
 		#include <LUFA/Platform/Platform.h>
 
@@ -63,15 +64,19 @@
 		/** LED mask for the library LED driver, to indicate that an error has occurred in the USB interface. */
 		#define LEDMASK_USB_ERROR        (LEDS_LED1 | LEDS_LED3)
 
+		/** LED mask for the library LED driver, to indicate that the USB interface is busy. */
+		#define LEDMASK_USB_BUSY          LEDS_LED2
+
 	/* Function Prototypes: */
 		void SetupHardware(void);
+		void MassStorageHost_Task(void);
 
-		void EVENT_USB_Device_Connect(void);
-		void EVENT_USB_Device_Disconnect(void);
-		void EVENT_USB_Device_ConfigurationChanged(void);
-		void EVENT_USB_Device_ControlRequest(void);
-
-		void EVENT_CDC_Device_LineEncodingChanged(USB_ClassInfo_CDC_Device_t* const CDCInterfaceInfo);
+		void EVENT_USB_Host_HostError(const uint8_t ErrorCode);
+		void EVENT_USB_Host_DeviceAttached(void);
+		void EVENT_USB_Host_DeviceUnattached(void);
+		void EVENT_USB_Host_DeviceEnumerationFailed(const uint8_t ErrorCode,
+		                                            const uint8_t SubErrorCode);
+		void EVENT_USB_Host_DeviceEnumerationComplete(void);
 
 #endif
 
