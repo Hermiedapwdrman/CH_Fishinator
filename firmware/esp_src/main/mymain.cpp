81b612508c582868ab6c461f15dadd7018fed534
patrick <re.craighospital@gmail.org>
2018-02-08 13:00:55 -0700

    Diactivated arduino init, working peripherial commands for test. Solenoid, reel, move cast location. no working implementaion of rod motor.

--- firmware/esp_src/main/mymain.cpp
+++ firmware/esp_src/main/mymain.cpp
@@ -31,21 +31,40 @@ void control_comm_task(void* novars); //Monitor incoming com task
 /** Encoder Setup **/
 AMT20_ABSQUADENC_SPI Aencoder(&SPI);
 volatile DRAM_ATTR int32_t esp_quadenc_position = 0;  //Encoder global counter for esp quadrature counting
-int16_t AMT20_abs_position = -1;
+DRAM_ATTR int16_t AMT20_abs_position = -1;
+DRAM_ATTR int32_t roboclaw_position = 0;
 
 /** Roboclaw setup **/
-#define address 0x80
+#define roboclaw_addr 0x80
 HardwareSerial HWSerial(1);
 RoboClaw roboclaw(&HWSerial,10000);
 
 
 /**Fishing rod testing functions **/
 const int32_t rod_cast_begin_pos = 3450;
-const int32_t rod_cast_release_pos = 2600;
+int32_t rod_cast_release_pos = 2600;
 const int32_t rod_cast_finish_pos = 1250;
 const int32_t rod_hook_pos = 2400;
 const int32_t rod_neutral_pos = 1760;
+const int32_t reel_pwm_drive = 0x4000;
+
 void fishing_rod_cast_sequence();
+
+void sync_encoders(){
+    AMT20_abs_position = Aencoder.readEncoderPosition();
+    esp_quadenc_position = AMT20_abs_position;
+    roboclaw.SetEncM1(roboclaw_addr,AMT20_abs_position);
+}
+
+/** Solenoid GPI Config **/
+const gpio_num_t solenoid_gpio_pin = GPIO_NUM_2;
+const gpio_config_t Solenoid_GPIOconfig{ //Define Encoder B behavior, pin selection is arbitrary and might change
+      .pin_bit_mask = (1<<solenoid_gpio_pin),
+      .mode = GPIO_MODE_OUTPUT,
+      .pull_up_en = GPIO_PULLUP_DISABLE,
+      .pull_down_en = GPIO_PULLDOWN_DISABLE,
+      .intr_type = GPIO_INTR_DISABLE
+};
 
 
 /**
@@ -59,10 +78,11 @@ extern "C" void app_main()
 
     printf("Testing GPIO interrupts for encoder counting.!\n");
     //TESTING using easier to implement arduino serial.
-    Serial.begin(921600); //Use arduino serial class for ease.
+//    Serial.begin(921600); //Use arduino serial class for ease.
 
     /** Trying arduino spi library **/
     SPI.begin();
+
     roboclaw.begin(460800);
 
 
@@ -70,27 +90,28 @@ extern "C" void app_main()
 // Set GPIO for encoder counting.
     gpio_config(&EncA_GPIOconfig);
     gpio_config(&EncB_GPIOconfig);
+    gpio_config(&Solenoid_GPIOconfig);
 
     //Install gpio interrupt service
     gpio_install_isr_service(ESP_INTR_FLAG_IRAM);
     gpio_isr_handler_add(EncA_GPIO, &AMT20enc_isr_handler, NULL);
     gpio_isr_handler_add(EncB_GPIO, &AMT20enc_isr_handler, NULL);
+    gpio_set_level(solenoid_gpio_pin,1);
+
 
 
     //Read abs encoder position and update
     do{
-        AMT20_abs_position = Aencoder.readEncoderPosition();
-        vTaskDelay(2/portTICK_PERIOD_MS);
-        AMT20_abs_position = Aencoder.readEncoderPosition();  //Why do we need to call twice?
-        esp_quadenc_position = AMT20_abs_position;
-        roboclaw.SetEncM1(address,AMT20_abs_position);
+        sync_encoders();
+        ets_delay_us(2000);
+        sync_encoders();
     }
     while(AMT20_abs_position == -1);
 
     //Create threads to handle program tasks.
 //    xTaskCreatePinnedToCore(&print_task, "print_task",configMINIMAL_STACK_SIZE,NULL,5,NULL,1);
-    xTaskCreate(&print_task,"Print_Task",4092,NULL,5,NULL);
-    xTaskCreate(&control_comm_task, "Comm task",4092,NULL, 10,NULL);
+    xTaskCreate(&print_task,"Print_Task",8192,NULL,1,NULL);
+    xTaskCreate(&control_comm_task, "Comm task",8192,NULL, 2,NULL);
 //    xTaskCreate(&spi_AMT_encoder_task,"Encoder Task",8192,NULL,5,NULL);
 
 
@@ -107,14 +128,14 @@ void print_task(void *novars) {
 
 //        AMT20_abs_position = read_AMT20_position();
         AMT20_abs_position = Aencoder.readEncoderPosition();
-        int32_t roboclaw_position = roboclaw.ReadEncM1(address, &RC_status, &RC_valid_flag);
+        roboclaw_position = roboclaw.ReadEncM1(roboclaw_addr, &RC_status, &RC_valid_flag);
 
         printf("Abs. Pos: %i \t\t", AMT20_abs_position);
         printf("Quad Pos: %i \t\t", esp_quadenc_position);
         printf("Roboclaw Quad: %i \n", roboclaw_position);
 
 //        esp_task_wdt_reset();
-        vTaskDelay(500 / portTICK_PERIOD_MS);
+        vTaskDelay(3000 / portTICK_PERIOD_MS);
     }
 }
 
@@ -122,23 +143,19 @@ void control_comm_task(void* novars){
 
     while(1) {
         int inchar = 0;
-//        int sanitycheck = 0;
-
-        if (Serial.available()) {
-            inchar = Serial.read();
+        inchar = getchar();
+        if (inchar != EOF) {
             printf("You sent: %c\n", inchar);
         }
-        vTaskDelay(30 / portTICK_PERIOD_MS);
-
 
         switch (inchar) {
             case 't':  //Re-calibrate encoders
-
-//                AMT20_abs_position = Aencoder.readEncoderPosition();
-//                vTaskDelay(2/portTICK_PERIOD_MS);
+                sync_encoders();
+////                AMT20_abs_position = Aencoder.readEncoderPosition();
+////                vTaskDelay(2/portTICK_PERIOD_MS);
 //                AMT20_abs_position = Aencoder.readEncoderPosition();  //Why do we need to call twice?
-                esp_quadenc_position = AMT20_abs_position;
-                roboclaw.SetEncM1(address,AMT20_abs_position);
+//                esp_quadenc_position = AMT20_abs_position;
+//                roboclaw.SetEncM1(roboclaw_addr,AMT20_abs_position);
                 break;
 
             case 'a': // Go to cast start
@@ -147,7 +164,12 @@ void control_comm_task(void* novars){
 
                 break;
 
-            case 'd': // Go to cast release location
+            case 's': // Go to cast release location
+
+
+
+                break;
+            case 'd': // Go to cast finish location
 
 
 
@@ -174,24 +196,32 @@ void control_comm_task(void* novars){
                 break;
 
             case '=': //Fire solenoid for 1 second
-
+                gpio_set_level(solenoid_gpio_pin,0);
                 break;
 
             case 'u': //Reel for 30ms?
-
+                roboclaw.DutyM2(roboclaw_addr, reel_pwm_drive);
                 break;
 
             case 'q': //Advanced cast release 5 counts
-
+                rod_cast_release_pos += 5;
+                printf("Cast Release: %i\n", rod_cast_release_pos);
                 break;
 
             case 'w': //Retard cast release 5 counts
-
+                rod_cast_release_pos -= 5;
+                printf("Cast Release: %i\n", rod_cast_release_pos);
                 break;
+
             default:
+                roboclaw.DutyM2(roboclaw_addr, 0);
+                gpio_set_level(solenoid_gpio_pin,1);
                 break;
 
         }
+
+        vTaskDelay(30 / portTICK_PERIOD_MS); //Sleep thread for 30ms
+
     }
 
 
