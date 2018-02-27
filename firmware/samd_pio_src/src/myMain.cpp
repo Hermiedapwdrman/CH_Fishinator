58086c11e6ca497c610446be684f46b83190bff9
patrick <re.craighospital@gmail.org>
2018-02-27 11:28:06 -0700

    Added exp to find serial ports, SerialUSB for usb device comm, Serial 1 for breakout on board.

--- firmware/samd_pio_src/src/myMain.cpp
+++ firmware/samd_pio_src/src/myMain.cpp
@@ -3,13 +3,19 @@
 //
 
 #include <Arduino.h>
-#include <Usb.h>
+//#include <Usb.h>
 
 
 int vled = 0;
+unsigned long next_time = 0;
 
 void setup(){
- pinMode(LED_BUILTIN,OUTPUT);
+    pinMode(LED_BUILTIN,OUTPUT);
+    Serial1.begin(115200);
+    Serial.begin(115200);
+    SERIAL_PORT_MONITOR.begin(115200);
+    SerialUSB.begin(115200);
+    next_time = millis();
 
 }
 
@@ -22,8 +28,11 @@ void loop(){
         digitalWrite(LED_BUILTIN,vled);
         vled = 0;
     }
+    delay(421);
 
-    delay(121);
-
+    Serial1.print(F("U5U5"));
+    Serial.print(F("X2X2"));
+    SERIAL_PORT_MONITOR.print(F("ABAB"));
+    SerialUSB.print(F("HGHG"));
 
 }
